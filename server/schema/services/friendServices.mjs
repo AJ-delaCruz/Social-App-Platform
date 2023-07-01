e61d575111abb915cee917dff3825637d0811c5b
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-06-30 22:55:59 -0700

    change db to postgres for friend model

--- server/schema/services/friendServices.mjs
+++ server/schema/services/friendServices.mjs
@@ -1,15 +1,13 @@
 import { v4 as uuidv4 } from 'uuid';
-import { cassandra } from '../../utils/db.mjs';
+import { pgDb } from '../../utils/db.mjs';
 
 // retrieve friends list of the user
 const getAllFriendsService = async (userId) => {
   try {
-    // Query to retrieve friends from the Cassandra database
-    const query = 'SELECT friend_id FROM social_media.friends WHERE user_id = ?';
-    const params = [userId];
-    // console.log(userId);
-    // Execute the query using the Cassandra client
-    const result = await cassandra.execute(query, params, { prepare: true });
+    // Query to retrieve friends from the PostgreSQL database
+    const query = 'SELECT friend_id FROM friends WHERE user_id = $1';
+    const result = await pgDb.query(query, [userId]);
+
     // console.log(result);
 
     // Extract friend IDs from the result
@@ -59,12 +57,10 @@ const sendFriendRequestService = async (userId, friendId) => {
     //   throw new Error('User is not found');
     // }
 
-    // query to insert the friend into Cassandra
-    const query = 'INSERT INTO social_media.friends (id, user_id, friend_id, created_at) VALUES (?, ?, ?, ?)';
-    // parameters for the query
-    const params = [id, userId, friendId, createdAt];
-    // Execute the query using the Cassandra client
-    await cassandra.execute(query, params, { prepare: true }); // prepare statement and cache
+    // query to insert the friend into PostgreSQL
+    const query = 'INSERT INTO social_media.friends (id, user_id, friend_id, created_at) VALUES ($1, $2, $3, $4)';
+    // Execute the query using the PostgreSQL client
+    await pgDb.query(query, [id, userId, friendId, createdAt]);
 
     // Return the friend object
     return {
@@ -84,20 +80,17 @@ const removeFriendService = async (userId, friendId) => {
 
   try {
     // Check if friendId exists in the user's friends list
-    const checkQuery = 'SELECT id FROM social_media.friends WHERE user_id = ? AND friend_id = ?';
-    const checkParams = [userId, friendId];
-    const checkResult = await cassandra.execute(checkQuery, checkParams, { prepare: true });
+    const checkQuery = 'SELECT id FROM friends WHERE user_id = $1 AND friend_id = $2';
+    const checkResult = await pgDb.query(checkQuery, [userId, friendId]);
     // error if friendId doesn't exist
     if (checkResult.rowLength === 0) {
       throw new Error('Friend relationship not found');
     }
 
     // query to remove friend
-    const query = 'DELETE FROM social_media.friends WHERE user_id = ? AND friend_id = ?';
-    // parameters for the query
-    const params = [userId, friendId];
-    // Execute the query using the Cassandra client
-    await cassandra.execute(query, params, { prepare: true }); // prepare statement and cache
+    const query = 'DELETE FROM friends WHERE user_id = $1 AND friend_id = $2';
+    // Execute the query using the PostgreSQL client
+    await pgDb.query(query, [userId, friendId]);
 
     // Return true
     return true;
