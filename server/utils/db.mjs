d3bc27445bbdfa9a87a387e7c4090fe7503423c2
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-07-01 23:44:16 -0700

    setup environment variables for database in the cloud

--- server/utils/db.mjs
+++ server/utils/db.mjs
@@ -4,23 +4,23 @@ import Redis from 'ioredis';
 
 // PostgreSQL database connection
 const pgDb = new pg.Client({
-  user: 'root',
-  host: 'localhost',
-  database: 'socialdb',
-  password: 'password',
-  port: 5432,
+  user: process.env.PG_USER || 'ajay',
+  host: process.env.PG_HOST || 'localhost',
+  database: process.env.PG_DB || 'socialdb',
+  password: process.env.PG_PASSWORD || 'password',
+  port: process.env.PG_PORT || 5432,
 });
 
 // Cassandra database connection
 const cassandra = new Client({
-  contactPoints: ['127.0.0.1'],
-  localDataCenter: 'datacenter1',
-  keyspace: 'social_media',
+  contactPoints: [process.env.CASSANDRA_CONTACT_POINT || '127.0.0.1'],
+  localDataCenter: process.env.CASSANDRA_DATA_CENTER || 'datacenter1',
+  keyspace: process.env.CASSANDRA_KEYSPACE || 'social_media',
 });
 // Redis database connection
 const redis = new Redis({
-  host: 'localhost',
-  port: 6379,
+  host: process.env.REDIS_HOST || 'localhost',
+  port: process.env.REDIS_PORT || 6379,
 });
 
 export { pgDb, cassandra, redis };
